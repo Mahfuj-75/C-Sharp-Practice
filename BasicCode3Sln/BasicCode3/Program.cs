3af3c2bd09a19089f7049200db6fbc216a3e7cd5
[Mahfuj Rahman] <[mahfuj.majumder2002@gmail.com]>
2026-03-08 07:46:58 +0600

    added basic code 3

---
+++ BasicCode3Sln/BasicCode3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode3
+{
+    class Account
+    {
+        private double balance;
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+                balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+                balance -= amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+    
+    internal class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            acc.Deposit(1000);
+            acc.Withdraw(500);
+            Console.WriteLine("Balance: " + acc.GetBalance());
+        }
+    }
+}
---
+++ BasicCode3Sln/BasicCode3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BasicCode3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicCode3")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d129efd8-35fb-4155-b6f1-ddc6347d9a8c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
