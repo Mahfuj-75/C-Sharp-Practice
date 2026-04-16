68aaf22d4c24bf79fd8bdff163c463be6364d6be
[Mahfuj Rahman] <[mahfuj.majumder2002@gmail.com]>
2026-04-16 10:49:26 +0600

    solve error and added library management system file

--- LibraryManagement/LMSSln/LMS/Program.cs
+++ LibraryManagement/LMSSln/LMS/Program.cs
@@ -21,12 +21,12 @@ namespace LMS
     }
     class Book : LibraryItem
     {
-        public string Author { get;set }
+        public string Author { get; set; }
         public Book(string title, int id, string author) : base(title, id)
         {
             Author = author;
         }
-        public overrride void Display()
+        public override void Display()
         {
             Console.WriteLine("Book ID: " + Id);
             Console.WriteLine("Title: " + Title);
@@ -71,6 +71,58 @@ namespace LMS
     {
         static void Main(string[] args)
         {
+            Library lib = new Library();
+
+            while(true)
+            {
+                Console.WriteLine("\n1. Add Book");
+                Console.WriteLine("2. Add Magazine");
+                Console.WriteLine("3. Show All Items");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter Choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                
+                if(choice==1)
+                {
+                    Console.Write("Enter Book ID: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter Title: ");
+                    string title = Console.ReadLine();
+
+                    Console.Write("Enter Author: ");
+                    string author = Console.ReadLine();
+
+                    lib.AddItem(new Book(title, id, author));
+
+                }
+                else if (choice ==2)
+                {
+                    Console.Write("Enter Magazine ID: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter Title: ");
+                    string title = Console.ReadLine();
+
+                    Console.Write("Enter Issue Number: ");
+                    int issue = Convert.ToInt32(Console.ReadLine());
+
+                    lib.AddItem(new Magazine(title, id, issue));
+                }
+                else if (choice==3)
+                {
+                    lib.ShowAllItems();
+                }
+                else if (choice==4)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid Chocie");
+                }
+            }
         }
     }
 }
